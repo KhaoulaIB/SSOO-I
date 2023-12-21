bb8be38efb895617e5fd0e074a8022f4cda6792e
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2023-12-21 11:39:30 +0100

    Update nivel2.c

--- Practica2/nivel2.c
+++ Practica2/nivel2.c
@@ -11,6 +11,7 @@
 #define DEBUGN2 1
 #define SUCCES 0
 #define FAILURE -1
+#define delimitadores " \t\n\r"
 #define RESET "\033[0m"
 #define NEGRO_T "\x1b[30m"
 #define NEGRO_F "\x1b[40m"
@@ -23,6 +24,7 @@
 #define CYAN_T "\x1b[36m"
 #define BLANCO_T "\x1b[97m"
 #define NEGRITA "\x1b[1m"
+
 
 char *read_line(char *line);
 int execute_line(char *line);
@@ -46,7 +48,7 @@ void BorrarChar(char * palabra, char c);
  *******************************************************************************/
 int main() {
 
-char line[COMMAND_LINE_SIZE];    
+char *line = (char *) malloc(sizeof(COMMAND_LINE_SIZE));
     while (1) {
         if (read_line(line)) {
             execute_line(line);
@@ -58,6 +60,8 @@ char line[COMMAND_LINE_SIZE];
 
     
 }
+
+
 
 
 /*!*****************************************************************************
@@ -110,10 +114,12 @@ char *read_line(char *line) {
  *******************************************************************************/
 
 int execute_line(char *line) {
-    char *args[ARGS_SIZE];
+   char **args = malloc(sizeof(char *) * ARGS_SIZE);
     if (parse_args(args, line) > 0) {    //parsea la línea en argumentos
         check_internal(args);
     }
+    memset(line, '\0', COMMAND_LINE_SIZE);
+    free(args);
     return 0;
 }
 
@@ -129,14 +135,14 @@ int execute_line(char *line) {
 int parse_args(char **args, char *line) {
     int numTokens = 0;
 
-    args[numTokens] = strtok(line, " \t\n\r");
+    args[numTokens] = strtok(line, delimitadores);
 #if DEBUGN1
     fprintf(stderr, GRIS_T "[parse_args()→ token %i: %s]\n" RESET , numTokens, args[numTokens]);
 #endif
     while (args[numTokens] && args[numTokens][0] != '#')
     { 
         numTokens++;
-        args[numTokens] = strtok(NULL, " \t\n\r");
+        args[numTokens] = strtok(NULL, delimitadores);
 #if DEBUGN1
         fprintf(stderr,  GRIS_T "[parse_args()→ token %i: %s]\n" RESET , numTokens, args[numTokens]);
 #endif
@@ -263,32 +269,19 @@ int internal_export(char **args) {
    char * nombre= (char *)malloc(sizeof(COMMAND_LINE_SIZE));
     char * valor = (char *)malloc(sizeof(COMMAND_LINE_SIZE));
 
+    nombre = strtok(args[1],"=");
+    valor = strtok(NULL,delimitadores);
 
-   int indexequal = -1;
-    for (int i = 0; i < strlen(args[1]); i++) {
-        if (args[1][i] == '=') {//sale cuando encuentra el primer igual
-            indexequal = i;
-            break;
-        }
-    }
-     if (indexequal == -1) { //si no existe el caracter '='
-        fprintf(stderr, ROJO_T "Sintaxis incorrecta. Uso: %s NOMBRE=VALOR\n", args[0]);
+     #if DEBUGN2
+    fprintf(stderr,GRIS_T"[internal_export()→ nombre: %s]\n"RESET,nombre);
+    fprintf(stderr,GRIS_T"[internal_export()→ valor: %s]\n"RESET,valor);
+    #endif
+    if (!nombre || !valor){
+         fprintf(stderr, ROJO_T "Sintaxis incorrecta. Uso: %s NOMBRE=VALOR\n", args[0]);
         return EXIT_FAILURE;
     }
    
-    // Obtener el nombre de la variable
-    strncpy(nombre, args[1], indexequal);
-    nombre[indexequal] = '\0'; // marcar el fin de la cadena
-
-    // Obtener el valor
-    int valor_length = strlen(args[1]) - indexequal - 1;
-    strncpy(valor, args[1] + indexequal + 1, valor_length);
-    valor[valor_length] = '\0'; 
-
-
     #if DEBUGN2
-    fprintf(stderr,GRIS_T"[internal_export()→ nombre: %s]\n"RESET,nombre);
-    fprintf(stderr,GRIS_T"[internal_export()→ valor: %s]\n"RESET,valor);
     fprintf(stderr,GRIS_T "[internal_export()→ antiguo valor para %s: %s]\n" RESET, nombre, getenv(nombre));
     #endif
     if (setenv(nombre, valor, 1) < 0) {
@@ -323,7 +316,6 @@ int internal_source(char **args){
 
 /*!*****************************************************************************
  * @brief Muestra el PID de procesos en background(segundo plano)
- * En este nivel, la función simplemente imprime su funcionamiento.
  * @param args Lista de argumentos (actualmente no utilizada).
  * @return 0
  *******************************************************************************/
