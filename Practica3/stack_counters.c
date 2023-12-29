d528705f84abf2040952253d5a052d8ed0dfa4c9
KhaoulaIB <115530714+KhaoulaIB@users.noreply.github.com>
2023-12-29 15:11:20 +0100

    Creación de la práctica + metodo worker

---
+++ Practica3/stack_counters.c
@@ -0,0 +1,28 @@
+#include <pthread.h>
+#include "my_lib.h"
+#define NUM_THREADS 10
+#define  N 1000000
+
+//mutex global
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; 
+//funciones
+void *worker(void *ptr);
+struct my_stack *pila;
+
+void *worker(void *ptr){
+    //añadir control de errores
+    for (int i = 0; i<N; i++){
+
+    pthread_mutex_lock(&mutex); 
+    int tmp =my_stack_pop();
+    pthread_mutex_unlock(&mutex);
+    tmp++; 
+    pthread_mutex_lock(&mutex);
+    my_stack_push(pila,tmp);
+    pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_exit(NULL);//salir de la función
+}
+
+
